abce944026e8ba1c44dfff617ac1c252a49022fb
HyreKaro <ssanjit856@gmail.com>
2024-08-09 11:18:42 +0530

    chore added knexfile.ts

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,24 +1,24 @@
-// import { ENV } from "./config/env.cofig";
+import { ENV } from "./config/env.cofig";
 
-// export default {
-//   development: {
-//     client: 'pg',
-//     connection: {
-//       host: ENV.POSTGRES_HOST,
-//       user: ENV.POSTGRES_USER,
-//       port: ENV.POSTGRES_PORT,
-//       password: ENV.POSTGRES_PASSWORD,
-//       database: ENV.POSTGRES_DB,
-//       ssl: {
-//         rejectUnauthorized: true,
-//         ca: ENV.POSTGRES_SSL_CA
-//       }
-//     },
-//     migrations: {
-//       directory: './migrations'
-//     },
-//     seeds: {
-//       directory: './seeds'
-//     }
-//   }
-// };
+export default {
+  development: {
+    client: 'pg',
+    connection: {
+      host: ENV.POSTGRES_HOST,
+      user: ENV.POSTGRES_USER,
+      port: ENV.POSTGRES_PORT,
+      password: ENV.POSTGRES_PASSWORD,
+      database: ENV.POSTGRES_DB,
+      ssl: {
+        rejectUnauthorized: true,
+        ca: ENV.POSTGRES_SSL_CA
+      }
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  }
+};
